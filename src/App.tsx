a66e3f4aa148e13ef50274f7e61def5c055d8616
Cristian Perez <cperezlecaros@gmail.com>
2025-03-16 06:39:21 -0700

    Create project structure and install tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Tasks App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
