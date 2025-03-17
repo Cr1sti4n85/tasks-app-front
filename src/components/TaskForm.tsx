cec451b27e455d92ea23aa079b7d61c63e8463a0
Cristian Perez <cperezlecaros@gmail.com>
2025-03-16 23:36:32 -0700

    Create Task Form ui

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,33 @@
+function TaskForm() {
+  return (
+    <div>
+      <form action="">
+        <input
+          type="text"
+          name="title"
+          placeholder="Write a title"
+          className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+        />
+        <textarea
+          className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+          name="description"
+          rows={3}
+          placeholder="Write a description"
+        ></textarea>
+        <label htmlFor="" className="inline-flex items-center gap-x-2">
+          <input
+            type="checkbox"
+            name="completed"
+            className="h-5 w-5 text-indigo-600"
+          />
+          <span>Done</span>
+        </label>
+        <button className="bg-indigo-500 px-3 py-2 w-full rounded-md">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
