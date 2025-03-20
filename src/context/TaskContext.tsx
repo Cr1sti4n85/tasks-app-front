b2561969a58d27da34b774f4f2a74c63073702df
Cristian Perez <cperezlecaros@gmail.com>
2025-03-20 15:29:01 -0700

    Create TaskContext

---
+++ src/context/TaskContext.tsx
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+interface ContextProps {
+  children: React.ReactNode;
+}
+
+const TaskContext = createContext({
+  tasks: [],
+});
+
+export const TaskProvider: React.FC<ContextProps> = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+  return (
+    <TaskContext.Provider value={{ tasks }}>{children}</TaskContext.Provider>
+  );
+};
